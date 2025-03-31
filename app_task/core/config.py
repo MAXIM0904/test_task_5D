82fb075922fe4e291344bfd6c055d61e4b46b482
Max <solodovnikov_mv@mail.ru>
2025-03-31 21:51:08 +0300

    test_task

--- app_task/core/config.py
+++ app_task/core/config.py
@@ -13,7 +13,7 @@ class ServiceSettings(BaseSettings):
     service_name: str = Field("TestTask", alias="NAME_SERVICE")
     host: str = Field("127.0.0.1", alias="START_HOST")
     port: int = Field(8080, alias="START_PORT")
-    token: str = Field('ad49c4f614f5fedd0b369c372303d4ab8829846e', alias="TOKEN_BITLY")
+    token: str = Field(alias="TOKEN_BITLY")
 
     model_config = SettingsConfigDict(env_file=file_path)
 
